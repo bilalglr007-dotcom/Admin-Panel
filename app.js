3b63a45f5b09dbbb6fbea4eb332c19064e74e970
Bilal Guler <bilalglr007@gmail.com>
2026-07-17 17:06:42 +0300

    Express Generator ESM altyapisi kuruldu

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-import express from 'express'
-const app = express()
-app.get('/api',(req,res)=>
-    {
-        res.json({Message:'Admin Panel API Çalışıyor!'})
-    })
-app.listen(8080,()=>
-    {
-        console.log('Sunucu Ayaklandı!')
-    })    
+import express  from 'express'
+import path from ('node:path')
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import indexRouter from('./routes/index.js');
+import usersRouter from ('./routes/users.js');
+
+const app = express();
+const __dirname = new URL('.', import.meta.url).pathname;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
