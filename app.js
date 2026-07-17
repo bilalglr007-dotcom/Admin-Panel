10221d9618b4761be48f8a52f90c6b65aa8cd21b
Bilal Guler <bilalglr007@gmail.com>
2026-07-17 17:49:44 +0300

    Merkezi hata yakalama mekanizmasi JSON formatina uyarlandi

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 import express  from 'express'
-import path from ('node:path')
+import path from 'node:path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-
-import indexRouter from('./routes/index.js');
-import usersRouter from ('./routes/users.js');
+import indexRouter from './routes/index.js'
+import usersRouter from './routes/users.js'
 
 const app = express();
 const __dirname = new URL('.', import.meta.url).pathname;
@@ -17,5 +16,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use((req,res,next) =>
+    {
+        const error = new Error('Sayfa Bulunamadı')
+        error.status = 404
+        next(error)
+    })
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  
+  res.status(statusCode).json({
+    error: {
+      message: error.message,
+      detail: app.get('env') === 'development' ? error.stack : {}
+    }
+  });
+});
 
 export default app;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', (req, res, next) => {
   res.json({message: 'respond with a resource'});
 });
