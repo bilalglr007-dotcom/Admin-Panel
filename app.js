cf05013121efb3411e0c0ed93e1d1cd3502952a8
Bilal Guler <bilalglr007@gmail.com>
2026-07-20 09:40:49 +0300

    Bazı değişiklikler uygulandı.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-import express  from 'express'
-import path from 'node:path'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
-import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
+import express from 'express';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
 const app = express();
-const __dirname = new URL('.', import.meta.url).pathname;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,19 +20,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use((req,res,next) =>
-    {
-        const error = new Error('Sayfa Bulunamadı')
-        error.status = 404
-        next(error)
-    })
+app.use((req, res, next) => {
+    const error = new Error('İstediğiniz API rotası bulunamadı.');
+    error.status = 404;
+    next(error);
+});
+
 app.use((error, req, res, next) => {
   const statusCode = error.status || 500;
   
   res.status(statusCode).json({
+    success: false,
     error: {
       message: error.message,
-      detail: app.get('env') === 'development' ? error.stack : {}
+      detail: app.get('env') === 'development' ? error.stack : null 
     }
   });
 });
