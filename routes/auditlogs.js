bc50396d28101334484d01065efb7db5c75ceb8d
Bilal Guler <bilalglr007@gmail.com>
2026-07-21 17:55:02 +0300

    Rotaların  CRUD İşlemleri Tamamlandı

--- routes/auditlogs.js
+++ routes/auditlogs.js
@@ -1,9 +1,50 @@
-import express from 'express'
-const router = express.Router()
+import AuditLogs from '../db/models/auditlogs.js';
+import express from 'express';
+const router = express.Router();
 
-router.get('/',(req, res, next) =>
-    {
-        res.json({success: true, message: 'Audit Logs Route Başarıyla Çalışıyor'})
-    })
+router.post('/', async (req, res, next) => {
+  try {
+    const addAuditLog = await AuditLogs.create(req.body);
+    res.status(201).json({ success: true, data: addAuditLog });
+  } catch (err) {
+    next(err);
+  }
+});
 
-export default router;    
+router.get('/', async (req, res, next) => {
+  try {
+    const findAuditLogs = await AuditLogs.find();
+    res.status(200).json({ success: true, data: findAuditLogs });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const auditLogByDetail = await AuditLogs.findById(req.params.id);
+    res.status(200).json({ success: true, data: auditLogByDetail });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const auditLogUpdate = await AuditLogs.findByIdAndUpdate(req.params.id, req.body, { returnDocument: 'after' });
+    res.status(200).json({ success: true, data: auditLogUpdate });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const auditLogDelete = await AuditLogs.findByIdAndDelete(req.params.id);
+    res.status(200).json({ success: true, message: 'Sistem Günlüğü Başarıyla Silindi' });
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,51 @@
+import Categories from '../db/models/categories.js'
 import express from 'express'
 const router = express.Router()
 
-router.get('/',(req, res, next) => 
-    {
-        res.json({success: true, message: 'Categories Route Başarıyla Çalışıyor'})
-    })
+router.post('/', async (req, res, next) => {
+  try {
+    const addCategory = await Categories.create(req.body)
+    res.status(201).json({ success: true, data: addCategory })
+  } catch (err) {
+    next(err)
+  }
+})
 
-export default router;    
+router.get('/', async (req, res, next) => {
+  try {
+    const findCategories = await Categories.find()
+    res.status(200).json({ success: true, data: findCategories })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    // Burada Roles yerine Categories kullanıyoruz
+    const categoryByDetail = await Categories.findById(req.params.id)
+    res.status(200).json({ success: true, data: categoryByDetail })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const categoryUpdate = await Categories.findByIdAndUpdate(req.params.id, req.body, { returnDocument: 'after' })
+    res.status(200).json({ success: true, data: categoryUpdate })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const categoryDelete = await Categories.findByIdAndDelete(req.params.id)
+    res.status(200).json({ success: true, message: `${categoryDelete.name} Kategorisi Başarıyla Silindi`})
+  } catch (err) {
+    next(err)
+  }
+})
+
+export default router
