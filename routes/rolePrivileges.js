4a5e96e446c5bad70cb08ca7d59d148cd82792dc
Bilal Guler <bilalglr007@gmail.com>
2026-07-21 16:46:33 +0300

    Rotaların  CRUD İşlemleri Tamamlanıyor

--- routes/rolePrivileges.js
+++ routes/rolePrivileges.js
@@ -1,9 +1,50 @@
+import RolePrivileges from '../db/models/rolePrivileges.js'
 import express from 'express'
 const router = express.Router()
 
-router.get('/',(req, res, next) =>
-    {
-        res.json({success: true, message: 'Role Privileges Route Başarıyla Çalışıyor'})
-    })
-
+router.post('/', async (req, res, next) =>
+{
+  try {
+    const addRolePrivileges = await RolePrivileges.create(req.body)
+    res.status(201).json({success: true, data: addRolePrivileges})
+  } catch (err) {
+    next(err)
+  }
+})
+router.get('/',async (req,res,next) =>
+{
+  try {
+    const findRolePrivileges = await RolePrivileges.find()
+    res.status(200).json({success: true, data: findRolePrivileges})
+  } catch (err) {
+    next(err)
+  }
+})
+router.get('/:id',async (req,res,next) =>
+{
+  try {
+    const RolePrivilegesByDetail = await RolePrivileges.findById(req.params.id)
+    res.status(200).json({success: true, data: RolePrivilegesByDetail})
+  } catch (err) {
+    next(err)
+  }
+})
+router.put('/:id',async (req,res,next) =>
+{
+  try {
+    const RolePrivilegesUpdate = await RolePrivileges.findByIdAndUpdate(req.params.id, req.body, {returnDocument: 'after'})
+    res.status(200).json({success: true, data: RolePrivilegesUpdate})
+  } catch (err) {
+    next(err)
+  }
+})
+router.delete('/:id',async (req,res,next) =>
+{
+  try {
+    const  RolePrivilegesDelete = await RolePrivileges.findByIdAndDelete(req.params.id)
+    res.status(200).json({success: true, message: 'Rol-Yetki İlişkisi Başarıyla Silindi' })
+  } catch (err) {
+    next(err)
+  }
+})
 export default router;    
