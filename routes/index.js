7b428a32c71eeca5727180a629b67bab671dab6d
Bilal Guler <bilalglr007@gmail.com>
2026-07-22 14:24:41 +0300

    JWT Tabanlı Giriş/Kayıt Sistemi Yapıldı.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import config from '../config/index.js';
+import authRouter from '../routes/auth.js'
 const router = express.Router();
 
 router.get('/', (req, res, next) => 
@@ -11,5 +12,7 @@ router.get('/config-test',(req, res, next)=>
   {
     res.json({success: true, data: config})
   })
+
+  router.use('/auth', authRouter)
 
 export default router
